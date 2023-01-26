ef48a2f9b12400d7e2a3f90127b77122f7c8e230
Виктор Антонов <ortkivnovotna@yandex.ru>
2023-01-26 21:31:26 +0300

    fix user model, user manager

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,26 +1,44 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Необходимо указать email')
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_active') is not True:
+            raise ValueError('Superuser must have is_active=True.')
+        return self.create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
-    first_name = models.CharField(
-        max_length=150,
-        blank=False,
-        verbose_name='Имя'
-    )
-    last_name = models.CharField(
-        max_length=150,
-        blank=False,
-        verbose_name='Фамилия'
-    )
     email = models.EmailField(
-        unique=True,
         max_length=254,
+        unique=True,
         verbose_name='email address'
     )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ('username',)
+
+    objects = CustomUserManager()
 
     class Meta:
         verbose_name = 'Пользователь'
@@ -33,7 +51,7 @@ class User(AbstractUser):
         ]
 
     def __str__(self) -> str:
-        return f'{self.author} - {self.user}'
+        return self.email
 
 
 class Follow(models.Model):
