8e503dd1eeb8a91519724795b2be3a863ed6dcd9
Виктор Антонов <ortkivnovotna@yandex.ru>
2023-01-30 19:59:16 +0300

    fix

--- backend/foodgramm/urls.py
+++ backend/foodgramm/urls.py
@@ -2,13 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
 from ingredients.views import IngredientViewSet
 from recipes.views import RecipeViewSet
-from rest_framework.routers import DefaultRouter
 from tags.views import TagViewSet
 from users.views import MyUserViewSet, SubscribeViewSet, SubscriptionsViewSet
-
-router = DefaultRouter()
 
 router = DefaultRouter()
 router.register('ingredients', IngredientViewSet, basename='ingredients')
