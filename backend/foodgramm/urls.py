1d3b356f4ccac5d396e366e1acf90d0121082e40
Виктор Антонов <ortkivnovotna@yandex.ru>
2023-01-31 16:16:54 +0300

    fix urls

--- backend/foodgramm/urls.py
+++ backend/foodgramm/urls.py
@@ -3,33 +3,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from ingredients.views import IngredientViewSet
-from recipes.views import RecipeViewSet
-from rest_framework.routers import DefaultRouter
-from tags.views import TagViewSet
-from users.views import MyUserViewSet, SubscribeViewSet, SubscriptionsViewSet
-
-router = DefaultRouter()
-router.register('ingredients', IngredientViewSet, basename='ingredients')
-router.register('recipes', RecipeViewSet, basename='recipes')
-router.register('tags', TagViewSet, basename='tags')
-router.register('users', MyUserViewSet, basename='users')
+api_urlpatterns = [
+    path('', include('recipes.urls')),
+    path('', include('ingredients.urls')),
+    path('', include('tags.urls')),
+    path('', include('users.urls')),
+]
 
 urlpatterns = [
-    path(
-        'api/users/subscriptions/',
-        SubscriptionsViewSet.as_view({'get': 'list'}),
-        name='subscriptions'
-    ),
-    path(
-        'api/users/<int:user_id>/subscribe/',
-        SubscribeViewSet.as_view({'post': 'create', 'delete': 'destroy'}),
-        name='subscribe'
-    ),
-    path('api/', include(router.urls)),
-    path('api/', include('djoser.urls')),
+    path('api/', include(api_urlpatterns)),
     path('admin/', admin.site.urls),
-    path('api/auth/', include('djoser.urls.authtoken')),
 ]
 urlpatterns += static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
---
+++ backend/users/urls.py
@@ -0,0 +1,23 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import MyUserViewSet, SubscribeViewSet, SubscriptionsViewSet
+
+router = DefaultRouter()
+
+router.register('users', MyUserViewSet, basename='users')
+
+urlpatterns = [
+    path(
+        'users/subscriptions/',
+        SubscriptionsViewSet.as_view({'get': 'list'}),
+        name='subscriptions'
+    ),
+    path(
+        'users/<int:user_id>/subscribe/',
+        SubscribeViewSet.as_view({'post': 'create', 'delete': 'destroy'}),
+        name='subscribe'
+    ),
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+]
